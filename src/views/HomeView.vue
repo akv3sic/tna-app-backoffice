8d6d3fcf76b306ddae4066aa47984f86e8ee04ba
Ante Kvesić <antekvesic2@gmail.com>
2023-01-24 14:32:50 +0100

    Beautify home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,9 @@
   <div>
     <Header />
     <v-container fluid class="pa-0">
-      this is home :)
+      <v-img src="./../assets/img/rfid-card-reader.png" width="15%" class="center mt-9-prcnt"></v-img>
+      <h1 class="text-h4 text-center mt-4">Sustav za evidenciju prisutnosti</h1>
+      <p class="text-center text-subtitle-1 mt-7">Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br> Eaque error a molestias provident rerum? Dolore!</p>
     </v-container>
   </div>
 </template>
@@ -18,3 +20,17 @@ import Header from '@/components/layout/TheHeader'
     },
   }
 </script>
+
+
+<style>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+
+.mt-9-prcnt {
+  margin-top: 9%;
+}
+</style>
