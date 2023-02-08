c479d222a66c807764aa5fd07f3eecb41cebc6e1
Ante Kvesić <antekvesic2@gmail.com>
2023-02-08 13:11:55 +0100

    Edit home page

---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,27 @@
+<template>
+     <v-footer padless class="to-bottom">
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>Ugradbeni računalni sustavi</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+ export default {
+    name: "Footer",
+    data: () => ({
+        //
+    }),
+ }
+</script>
+
+<style>
+.to-bottom {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,18 +5,23 @@
       <v-img src="./../assets/img/rfid-card-reader.png" width="15%" class="center mt-9-prcnt"></v-img>
       <h1 class="text-h4 text-center mt-4">Sustav za evidenciju prisutnosti</h1>
       <p class="text-center text-subtitle-1 mt-7">Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br> Eaque error a molestias provident rerum? Dolore!</p>
+      <p class="text-center">
+        <a href="#" target="_blank">Korisnička dokumentacija</a>
+      </p>
     </v-container>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/layout/TheHeader'
+import Footer from '@/components/layout/TheFooter'
 
   export default {
     name: 'Home',
 
     components: {
-      Header
+      Header, Footer
     },
   }
 </script>
