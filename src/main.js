58f3f4c540e30cceedd1ebdebc25b7d40d96679a
Ante Kvesić <antekvesic2@gmail.com>
2023-02-13 23:33:02 +0100

    Add support for custom themes

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import lighttheme from '@/themes/lightTheme.js'
+import Vuetify from 'vuetify/lib'
+
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  vuetify: new Vuetify({
+    theme: {},
+  }),
+  render: (h) => h(App),
+}).$mount('#app');
+
---
+++ src/themes/lightTheme.js
@@ -0,0 +1,6 @@
+export default {
+    primary: '#00719A',
+    secondary: '#007F7D',
+    accent: '#8c9eff',
+    error: '#b71c1c'
+  }
